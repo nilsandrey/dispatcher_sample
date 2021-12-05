51439dceb3947a2f834df5aaaf1edebc7ce2e417
nils <nilsandrey@outlook.com>
2021-12-05 02:31:44 -0500

    Prevent loading state with low battery (model)
    Added logic in the model to prevent the drone from being in LOADING state if the battery level is below 25%.

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -4,6 +4,8 @@ class Drone < ApplicationRecord
   validates :serial_number, uniqueness: true
   validates :battery, numericality: { in: 0..100 }
 
+  before_update :check_on_update
+
   ##
   # Use model column as an enum
   enum model: %i[lightweight middleweight cruiserweight heavyweight]
@@ -12,4 +14,14 @@ class Drone < ApplicationRecord
   # Use state column as an enum with suffix for the values. (e.g. `idle_state`)
   # State has suffix because "loaded" interfere with already defined symbols (by the framework)
   enum state: %i[idle loading loaded delivering delivered returning], _suffix: true
+
+  ##
+  # Callback, put in before_update causes <tt>ActiveRecord::RecordNotSaved</tt>.
+  #
+  def check_on_update
+    if state_was != 'loading' && loading_state? && battery < 25 # TODO: Substitute with ENV var
+      errors.add(:state, 'Battery level to low for loading')
+      throw :abort
+    end
+  end
 end
--- test/models/drone_test.rb
+++ test/models/drone_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class DroneTest < ActiveSupport::TestCase
   test "Shouldn't allow long serial numbers" do
@@ -7,7 +7,7 @@ class DroneTest < ActiveSupport::TestCase
     assert_not d.valid?
   end
 
-  test 'Should allow serial numbers til 100 chars' do
+  test "Should allow serial numbers til 100 chars" do
     1.upto(100) do |i|
       d = drones(:first_drone)
       d.serial_number = random_string(i)
@@ -15,9 +15,17 @@ class DroneTest < ActiveSupport::TestCase
     end
   end
 
-  test 'Should not allow duplicate serial numbers' do
+  test "Should not allow duplicate serial numbers" do
     d = drones(:first_drone)
     d.serial_number = drones(:second_drone).serial_number
     assert_not d.valid?
+  end
+
+  test "Should throw an exception if try to change drone to loading state when battery level is below 25%" do
+    d = drones(:first_drone)
+    d.battery = 25 - 1 # TODO: Change 25 with ENV['BATTERY_LOW_LEVEL'], need to rebuild the container
+    d.save
+    assert_raises(ActiveRecord::RecordNotSaved) { d.loading_state! }
+    assert_equal(d.errors.messages.length, 1, 'Error message not recorded')
   end
 end
